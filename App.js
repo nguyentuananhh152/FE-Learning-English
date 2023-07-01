f1c524d43bea4dbd181f639ca1d2e33ee92e6877
dlinh2 <117155251+dlinh2@users.noreply.github.com>
2023-07-01 16:53:27 +0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,30 @@
+
+import './App.css';
+import Appbar from './components/AppBar';
+
+import {
+  BrowserRouter,
+  Route
+} from 'react-router-dom';
+import { Routes  } from 'react-router-dom';
+import ListCourse from './components/ListCourse';
+import ListLesson from './components/ListLesson';
+import Home from './components/Home';
+
+// import NotFound from "./components/NotFound";
+
+function App() {
+  return (
+    <div>
+      <BrowserRouter>
+            <Routes>
+              <Route exact path="/" element={<Home/>}/>
+              <Route exact path="/listcourse" element={<ListCourse/>}/>
+              <Route exact path="/listlesson" element={<ListLesson/>}/>
+            </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
---
+++ ListCourseStyle.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const divCourse = styled.div`
+{
+    width: 400px;
+    height: 500px;
+    background-color: white;
+    margin-right: 80px;
+}`
+
+export const imgCourse = styled.img`
+{
+    width: 100%;
+}`
